7aa0bbc4a860bbeeceeafbc38f251537e47ad120
Ismail <forwork.ismail@gmail.com>
2022-02-07 11:21:53 +0430

    Add Jwt auth guard to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/jwt/jwt-auth.guard';
 import databaseConfig from './config/database.config';
 import jwtConfig from './config/jwt.config';
 import serverConfig from './config/server.config';
@@ -18,6 +19,12 @@ import { TypeOrmConfig } from './config/typeorm.config';
       useClass: TypeOrmConfig
     }),
     AuthModule
+  ],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: JwtAuthGuard
+    }
   ]
 })
 export class AppModule {}
